614e05add546f64621c65fddbca68a7ba0cada48
JGH153 <jan.greger@gmail.com>
2023-10-18 12:08:57 +0200

    minor change

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,36 +1,21 @@
 import useSWR from "swr";
 import "./App.css";
 
-const fetcher = (url: string) =>
-  fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function App() {
-  const {
-    data,
-    error,
-    isLoading,
-    isValidating,
-  } = useSWR(
-    "https://random-data-api.com/api/v2/users",
-    fetcher
-  );
+  const { data, error, isLoading, isValidating } = useSWR("https://random-data-api.com/api/v2/users", fetcher);
 
   if (error) {
-    return <div>failed to load</div>;
-  }
-  if (isLoading) {
-    return <div>loading...</div>;
+    return <div>failed to load: {error}</div>;
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Hello {data.first_name}!
-        </h1>
-        {isValidating && (
-          <span>Updating...</span>
-        )}
+        {isLoading && <span>Loading...</span>}
+        {data && <h1>Hello {data.first_name}!</h1>}
+        {isValidating && !isLoading && <span>Updating...</span>}
       </header>
     </div>
   );
